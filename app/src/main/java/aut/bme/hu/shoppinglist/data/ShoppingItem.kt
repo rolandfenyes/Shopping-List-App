90e5d7b682e8a55991f52a38a05d900bdc05ee40
rolandfenyes <ftendalan@gmail.com>
2020-05-15 09:52:26 +0200

    ShoppingItemCategory enum added

--- app/src/main/java/aut/bme/hu/shoppinglist/data/ShoppingItem.kt
+++ app/src/main/java/aut/bme/hu/shoppinglist/data/ShoppingItem.kt
@@ -4,12 +4,18 @@ import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
+enum class ShoppingItemCategory {
+    FOOD,
+    ELECTRONIC,
+    BOOK
+}
+
 @Entity(tableName = "shoppingitem")
 data class ShoppingItem(
     @ColumnInfo(name = "id") @PrimaryKey(autoGenerate = true) var id: Long = 0,
 
     @ColumnInfo(name = "name") var name: String,
-    @ColumnInfo(name = "category") var category: String,
+    @ColumnInfo(name = "category") var category: ShoppingItemCategory,
     @ColumnInfo(name = "description") var description: String = "",
     @ColumnInfo(name = "estimated_price") var estimatedPrice: Int = 0,
     @ColumnInfo(name = "is_bought") var isBought: Boolean = false
