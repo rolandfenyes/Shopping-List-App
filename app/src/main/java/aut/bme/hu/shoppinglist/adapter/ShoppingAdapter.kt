d77f098693a9981e5a4d69bd97d6f592cfa2a254
rolandfenyes <ftendalan@gmail.com>
2020-05-15 10:35:51 +0200

    ShoppingAdapter class added

---
+++ app/src/main/java/aut/bme/hu/shoppinglist/adapter/ShoppingAdapter.kt
@@ -0,0 +1,25 @@
+package aut.bme.hu.shoppinglist.adapter
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class ShoppingAdapter: RecyclerView.Adapter<ShoppingAdapter.ShoppingViewHolder>() {
+
+    inner class ShoppingViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ShoppingViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: ShoppingViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+}
