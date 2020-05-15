9d065914ae7d65ec302162ff18cb6e2a5092c4c4
rolandfenyes <ftendalan@gmail.com>
2020-05-15 10:47:29 +0200

    ShoppingAdapter and item shopping list xml added

--- app/src/main/java/aut/bme/hu/shoppinglist/adapter/ShoppingAdapter.kt
+++ app/src/main/java/aut/bme/hu/shoppinglist/adapter/ShoppingAdapter.kt
@@ -1,21 +1,29 @@
 package aut.bme.hu.shoppinglist.adapter
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import aut.bme.hu.shoppinglist.R
+import aut.bme.hu.shoppinglist.data.ShoppingItem
 
 class ShoppingAdapter: RecyclerView.Adapter<ShoppingAdapter.ShoppingViewHolder>() {
+
+    private val items = mutableListOf<ShoppingItem>()
 
     inner class ShoppingViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
 
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ShoppingViewHolder {
-        TODO("Not yet implemented")
+        val itemView: View = LayoutInflater
+            .from(parent.context)
+            .inflate(R.layout.item_shopping_list, parent, false)
+        return ShoppingViewHolder(itemView)
     }
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        return  items.size
     }
 
     override fun onBindViewHolder(holder: ShoppingViewHolder, position: Int) {
