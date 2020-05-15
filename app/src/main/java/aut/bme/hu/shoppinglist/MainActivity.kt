848ba5da11a3431e7cbffb0a439d17f8d03fbe2e
rolandfenyes <ftendalan@gmail.com>
2020-05-15 11:34:27 +0200

    MainActivity is implemented

--- app/src/main/java/aut/bme/hu/shoppinglist/MainActivity.kt
+++ app/src/main/java/aut/bme/hu/shoppinglist/MainActivity.kt
@@ -2,11 +2,53 @@ package aut.bme.hu.shoppinglist
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import aut.bme.hu.shoppinglist.adapter.ShoppingAdapter
+import aut.bme.hu.shoppinglist.data.ShoppingItem
+import aut.bme.hu.shoppinglist.data.ShoppingListDatabase
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity: AppCompatActivity(), ShoppingAdapter.ShoppingItemClickListener, CoroutineScope by MainScope() {
+
+    private lateinit var database: ShoppingListDatabase
+    private lateinit var adapter: ShoppingAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+
+        database = ShoppingListDatabase.getDatabase(applicationContext)
+
+        fab.setOnClickListener {
+            // TODO
+        }
+
+        initRecyclerView()
+    }
+
+    override fun onItemChanged(item: ShoppingItem) {
+        updateItemInBackgound(item)
+    }
+
+    private fun updateItemInBackgound(item: ShoppingItem) = launch {
+        withContext(Dispatchers.IO) {
+            database.shoppingItemDao().update(item)
+        }
+    }
+
+    private fun initRecyclerView() {
+        adapter = ShoppingAdapter(this)
+        rvMain.layoutManager = LinearLayoutManager(this)
+        rvMain.adapter = adapter
+        loadItemsInBackground()
+    }
+
+    private fun loadItemsInBackground() = launch {
+        val items = withContext(Dispatchers.IO) {
+            database.shoppingItemDao().getAll()
+        }
+        adapter.update(items)
     }
 }
